861f9f798e6e029bc8167e712a7f5ba6e92c4d94
Cody <cody.koelliker@outlook.com>
2022-06-07 19:11:21 -0600

    make entities using SimpleGeoRenderer have a entityTranslucent rendertype

--- src/main/java/com/anar4732/croodaceous/client/render/SimpleGeoRenderer.java
+++ src/main/java/com/anar4732/croodaceous/client/render/SimpleGeoRenderer.java
@@ -1,13 +1,19 @@
 package com.anar4732.croodaceous.client.render;
 
 import com.anar4732.croodaceous.client.model.SimpleGeoModel;
+import com.anar4732.croodaceous.common.entities.LiyoteEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+import javax.annotation.Nullable;
 
 public class SimpleGeoRenderer<T extends LivingEntity & IAnimatable> extends GeoEntityRenderer<T> {
 	private float scale = 1F;
@@ -37,5 +43,9 @@ public class SimpleGeoRenderer<T extends LivingEntity & IAnimatable> extends Geo
 		}
 		super.render(entity, entityYaw, partialTicks, stack, bufferIn, packedLightIn);
 	}
-	
+
+	@Override
+	public RenderType getRenderType(T animatable, float partialTicks, PoseStack stack, @Nullable MultiBufferSource renderTypeBuffer, @Nullable VertexConsumer vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+		return RenderType.entityTranslucent(textureLocation);
+	}
 }
