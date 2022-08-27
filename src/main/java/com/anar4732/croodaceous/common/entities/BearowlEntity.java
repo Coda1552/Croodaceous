e29f1888433b949577ef40da4f3ffd62a2160752
Anar Abdullayev <qarabala4732@gmail.com>
2022-08-27 23:35:19 +0400

    When there are 2 bear owls in an area, they should occasionally ‘fight’ (but do no damage)

--- src/main/java/com/anar4732/croodaceous/common/entities/BearowlEntity.java
+++ src/main/java/com/anar4732/croodaceous/common/entities/BearowlEntity.java
@@ -52,6 +52,7 @@ public class BearowlEntity extends Animal implements IAnimatable {
 		this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.0D));
 		this.goalSelector.addGoal(2, new RandomLookAroundGoal(this));
 		this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, LivingEntity.class, 10, true, false, this::isTarget));
+		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, BearowlEntity.class, 10000, false, false, e -> true));
 	}
 	
 	public static AttributeSupplier.Builder createAttributes() {
@@ -212,6 +213,10 @@ public class BearowlEntity extends Animal implements IAnimatable {
 	@Override
 	public boolean hurt(DamageSource pSource, float pAmount) {
 		this.sleeping = false;
+		if (pSource.getEntity() instanceof BearowlEntity) {
+			this.knockback(0.5F, pSource.getEntity().getX() - this.getX(), pSource.getEntity().getZ() - this.getZ());
+			return false;
+		}
 		return super.hurt(pSource, pAmount);
 	}
 	
