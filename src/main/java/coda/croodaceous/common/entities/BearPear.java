c36fbfc82786da80c2e39df732c659a1e3f7d981
Skyler James <15920401+skyjay1@users.noreply.github.com>
2023-03-10 17:18:09 -0700

    Bear Pear cont.
    Bear Pear looks away from players until aggressive, then looks at players and attacks when in range.

--- src/main/java/coda/croodaceous/common/entities/BearPear.java
+++ src/main/java/coda/croodaceous/common/entities/BearPear.java
@@ -12,14 +12,16 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.Pose;
@@ -27,8 +29,12 @@ import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -48,17 +54,14 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
 import javax.annotation.Nullable;
+import java.util.EnumSet;
+import java.util.Optional;
 
 public class BearPear extends Animal implements IAnimatable {
 
+    private static final EntityDataAccessor<Optional<BlockPos>> DATA_HANGING_POS = SynchedEntityData.defineId(BearPear.class, EntityDataSerializers.OPTIONAL_BLOCK_POS);
+
     private static final TagKey<Block> SUPPORTS_BEAR_PEAR = BlockTags.LEAVES;
-
-    private static final EntityDataAccessor<Boolean> DATA_HANGING = SynchedEntityData.defineId(BearPear.class, EntityDataSerializers.BOOLEAN);
-
-    @Nullable
-    private BlockPos hangingPos;
-    private float hangingDis0;
-    private float hangingDis;
 
     // ANIMATIONS //
     private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
@@ -73,7 +76,8 @@ public class BearPear extends Animal implements IAnimatable {
                 .add(Attributes.MAX_HEALTH, 8.0D)
                 .add(Attributes.ATTACK_DAMAGE, 1.0D)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 1.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.2D);
+                .add(Attributes.FOLLOW_RANGE, 8.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.0D);
     }
 
     public static boolean canSpawn(EntityType<? extends BearPear> entityType, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
@@ -84,14 +88,18 @@ public class BearPear extends Animal implements IAnimatable {
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
-        getEntityData().define(DATA_HANGING, false);
+        getEntityData().define(DATA_HANGING_POS, Optional.empty());
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 6.0F)); // TODO look opposite of player to "hide"
-        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this)); // TODO only random look at night
+        this.goalSelector.addGoal(1, new BearPear.AttackGoal(this, 1.5F));
+        this.goalSelector.addGoal(5, new BearPear.LookAwayFromPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(5, new BearPear.LookAwayFromPlayerGoal(this, Jackrobat.class, 8.0F));
+        this.goalSelector.addGoal(6, new BearPear.RandomLookGoal(this));
+        this.targetSelector.addGoal(0, new HurtByTargetGoal(this).setAlertOthers());
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true, false));
     }
 
     @Override
@@ -127,18 +135,18 @@ public class BearPear extends Animal implements IAnimatable {
     @Override
     public void tick() {
         super.tick();
-        boolean isHanging = isHanging();
+        Optional<BlockPos> hangingPos = getHangingPos();
         // update position and movement
-        if(isHanging) {
+        if(hangingPos.isPresent()) {
             setDeltaMovement(Vec3.ZERO);
-            this.setPosRaw(this.getX(), (double) Mth.floor(this.getY()) - (double)this.getBbHeight(), this.getZ());
-
+            Vec3 hangingVec = Vec3.atBottomCenterOf(hangingPos.get()).subtract(0, 1.0F + getBbHeight(), 0);
+            setPosRaw(hangingVec.x(), hangingVec.y(), hangingVec.z());
         }
         // update hanging position
         if(!level.isClientSide() && (tickCount + getId()) % 20 == 1) {
-            if(isHanging) {
+            if(hangingPos.isPresent()) {
                 // validate hanging position
-                if(!canHangOn(hangingPos)) {
+                if(!canHangOn(hangingPos.get())) {
                     setHangingPos(null);
                 }
             } else {
@@ -165,18 +173,17 @@ public class BearPear extends Animal implements IAnimatable {
 
     //// HANGING ////
 
-    public BlockPos getHangingPos() {
-        return hangingPos;
+    public Optional<BlockPos> getHangingPos() {
+        return getEntityData().get(DATA_HANGING_POS);
     }
 
     public void setHangingPos(@Nullable BlockPos hangingPos) {
-        this.hangingPos = hangingPos;
+        this.getEntityData().set(DATA_HANGING_POS, Optional.ofNullable(hangingPos));
         this.setNoGravity(hangingPos != null);
-        this.getEntityData().set(DATA_HANGING, hangingPos != null);
     }
 
     public boolean isHanging() {
-        return getEntityData().get(DATA_HANGING);
+        return getHangingPos().isPresent();
     }
 
     public boolean canHangOn(final BlockPos pos) {
@@ -203,6 +210,11 @@ public class BearPear extends Animal implements IAnimatable {
 
     @Override
     protected void doPush(Entity pEntity) {
+        if ((pEntity instanceof Player entity)
+                && TargetingConditions.DEFAULT.test(this, entity)
+                && (null == this.getTarget() || this.getRandom().nextInt(10) == 0)) {
+            this.setTarget((LivingEntity)pEntity);
+        }
         return;
     }
 
@@ -248,9 +260,7 @@ public class BearPear extends Animal implements IAnimatable {
     @Override
     public void addAdditionalSaveData(CompoundTag pCompound) {
         super.addAdditionalSaveData(pCompound);
-        if(hangingPos != null) {
-            pCompound.put(KEY_HANGING_POS, NbtUtils.writeBlockPos(getHangingPos()));
-        }
+        getHangingPos().ifPresent(blockPos -> pCompound.put(KEY_HANGING_POS, NbtUtils.writeBlockPos(blockPos)));
     }
 
     //// ANIMATIONS ////
@@ -270,7 +280,173 @@ public class BearPear extends Animal implements IAnimatable {
         return factory;
     }
 
+    //// OTHER ////
+
+
     //// GOALS ////
 
+    protected void lookAwayFrom(final Entity entity) {
+        Vec3 deltaVec = entity.getEyePosition().subtract(this.getEyePosition());
+        Vec3 lookAtVec = this.getEyePosition().subtract(deltaVec);
+        this.getLookControl().setLookAt(lookAtVec.x(), lookAtVec.y(), lookAtVec.z());
+    }
+
+    private static class LookAwayFromPlayerGoal extends LookAtPlayerGoal {
+
+        private final BearPear entity;
+
+        public LookAwayFromPlayerGoal(BearPear pMob, Class<? extends LivingEntity> pLookAtType, float pLookDistance) {
+            super(pMob, pLookAtType, pLookDistance, 1.0F);
+            this.entity = pMob;
+        }
+
+        @Override
+        public boolean requiresUpdateEveryTick() {
+            return true;
+        }
+
+        @Override
+        public boolean canUse() {
+            return /*!this.mob.level.isNight() && */super.canUse();
+        }
+
+        @Override
+        public void tick() {
+            if(this.lookAt != null && this.lookAt.isAlive()) {
+                this.entity.lookAwayFrom(this.lookAt);
+                --this.lookTime;
+            }
+        }
+
+        @Override
+        public void start() { }
+
+        @Override
+        public void stop() {
+            this.lookAt = null;
+        }
+    }
+
+    private static class AttackGoal extends Goal {
+
+        private final BearPear mob;
+        private final float lookAtDistanceSq;
+
+        public int ticksUntilNextAttack;
+        private final int attackInterval = 20;
+        private long lastCanUseCheck;
+        private static final long COOLDOWN_BETWEEN_CAN_USE_CHECKS = 20L;
+
+        public AttackGoal(BearPear pMob, float lookAtDistance) {
+            this.mob = pMob;
+            this.lookAtDistanceSq = lookAtDistance * lookAtDistance;
+            setFlags(EnumSet.of(Flag.LOOK));
+        }
+
+        @Override
+        public boolean requiresUpdateEveryTick() {
+            return true;
+        }
+
+        @Override
+        public boolean canUse() {
+            long i = this.mob.level.getGameTime();
+            if (i - this.lastCanUseCheck < COOLDOWN_BETWEEN_CAN_USE_CHECKS) {
+                return false;
+            } else {
+                this.lastCanUseCheck = i;
+                LivingEntity livingentity = this.mob.getTarget();
+                if (livingentity == null) {
+                    return false;
+                } else if (!livingentity.isAlive()) {
+                    return false;
+                } else {
+                    return this.getAttackReachSqr(livingentity) >= this.mob.getEyePosition().distanceToSqr(livingentity.getX(), livingentity.getEyeY(), livingentity.getZ());
+                }
+            }
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            LivingEntity livingentity = this.mob.getTarget();
+            if (livingentity == null) {
+                return false;
+            } else if (!livingentity.isAlive()) {
+                return false;
+            } else {
+                return !(livingentity instanceof Player) || !livingentity.isSpectator() && !((Player)livingentity).isCreative();
+            }
+        }
+
+        @Override
+        public void start() {
+            this.mob.setAggressive(true);
+            this.ticksUntilNextAttack = 0;
+        }
+
+        @Override
+        public void stop() {
+            LivingEntity livingentity = this.mob.getTarget();
+            if (!EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(livingentity)) {
+                this.mob.setTarget(null);
+            }
+            this.mob.setAggressive(false);
+        }
+
+        @Override
+        public void tick() {
+            LivingEntity livingentity = this.mob.getTarget();
+            if (livingentity != null) {
+
+                this.mob.getLookControl().setLookAt(livingentity, 30.0F, 30.0F);
+                double disSq = this.mob.distanceToSqr(livingentity.getX(), livingentity.getEyeY(), livingentity.getZ());
+                if(disSq < lookAtDistanceSq) {
+                    this.mob.getLookControl().setLookAt(livingentity, 30.0F, 30.0F);
+                } else {
+                    this.mob.lookAwayFrom(livingentity);
+                }
+
+                this.ticksUntilNextAttack = Math.max(this.ticksUntilNextAttack - 1, 0);
+                this.checkAndPerformAttack(livingentity, disSq);
+            }
+        }
+
+        protected void checkAndPerformAttack(LivingEntity pEnemy, double pDistToEnemySqr) {
+            double d0 = this.getAttackReachSqr(pEnemy);
+            if (pDistToEnemySqr <= d0 && isTimeToAttack()) {
+                this.resetAttackCooldown();
+                this.mob.swing(InteractionHand.MAIN_HAND);
+                this.mob.doHurtTarget(pEnemy);
+            }
+
+        }
+
+        protected void resetAttackCooldown() {
+            this.ticksUntilNextAttack = this.adjustedTickDelay(attackInterval);
+        }
+
+        protected boolean isTimeToAttack() {
+            return this.ticksUntilNextAttack <= 0;
+        }
+
+        protected double getAttackReachSqr(LivingEntity pAttackTarget) {
+            return (this.mob.getBbWidth() * 2.0F * this.mob.getBbWidth() * 2.0F + pAttackTarget.getBbWidth());
+        }
+    }
+
+    private static class RandomLookGoal extends RandomLookAroundGoal {
+
+        private final Mob mob;
+
+        public RandomLookGoal(Mob pMob) {
+            super(pMob);
+            this.mob = pMob;
+        }
+
+        @Override
+        public boolean canUse() {
+            return !this.mob.level.isDay() && super.canUse();
+        }
+    }
 
 }
