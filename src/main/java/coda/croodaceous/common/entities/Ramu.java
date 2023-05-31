b888fb055555261180a94da3e7ff94beb966d99b
Cody <cody.koelliker@outlook.com>
2023-05-30 22:14:02 -0600

    fixed ramus sinking

--- src/main/java/coda/croodaceous/common/entities/Ramu.java
+++ src/main/java/coda/croodaceous/common/entities/Ramu.java
@@ -70,6 +70,7 @@ public class Ramu extends Animal implements IAnimatable {
 	
 	@Override
 	protected void registerGoals() {
+		this.goalSelector.addGoal(0, new FloatGoal(this));
 		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true));
 		this.goalSelector.addGoal(2, new RamuBreedGoal(this, 1.0D));
 		this.goalSelector.addGoal(3, new WaterAvoidingRandomStrollGoal(this, 1.0D));
