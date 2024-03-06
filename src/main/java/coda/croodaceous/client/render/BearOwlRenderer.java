ac78bc35925ec6ee11df89f5067938efe4ab56bc
0rc1nus <mitchellctlui@gmail.com>
2024-03-06 22:34:31 +0800

    Fixed Animation Parsing Errors

--- src/main/java/coda/croodaceous/client/render/BearOwlRenderer.java
+++ src/main/java/coda/croodaceous/client/render/BearOwlRenderer.java
@@ -3,7 +3,11 @@ package coda.croodaceous.client.render;
 import coda.croodaceous.client.render.layer.BearowlEyesLayer;
 import coda.croodaceous.client.model.BearowlModel;
 import coda.croodaceous.common.entities.Bearowl;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class BearOwlRenderer extends GeoEntityRenderer<Bearowl> {
@@ -11,5 +15,10 @@ public class BearOwlRenderer extends GeoEntityRenderer<Bearowl> {
 	public BearOwlRenderer(EntityRendererProvider.Context mgr) {
 		super(mgr, new BearowlModel());
 		addRenderLayer(new BearowlEyesLayer(this));
+	}
+
+	@Override
+	public RenderType getRenderType(Bearowl animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
+		return RenderType.entityCutout(texture);
 	}
 }
