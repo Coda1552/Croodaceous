d80d935250ef4487a68d7aab868d34f9cc56ab12
0rc1nus <mitchellctlui@gmail.com>
2024-03-06 22:36:51 +0800

    Update BearOwlRenderer.java

--- src/main/java/coda/croodaceous/client/render/BearOwlRenderer.java
+++ src/main/java/coda/croodaceous/client/render/BearOwlRenderer.java
@@ -3,6 +3,7 @@ package coda.croodaceous.client.render;
 import coda.croodaceous.client.render.layer.BearowlEyesLayer;
 import coda.croodaceous.client.model.BearowlModel;
 import coda.croodaceous.common.entities.Bearowl;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -15,6 +16,14 @@ public class BearOwlRenderer extends GeoEntityRenderer<Bearowl> {
 	public BearOwlRenderer(EntityRendererProvider.Context mgr) {
 		super(mgr, new BearowlModel());
 		addRenderLayer(new BearowlEyesLayer(this));
+	}
+
+	@Override
+	public void render(Bearowl entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+		if (entity.isBaby()) {
+			poseStack.scale(0.5F, 0.5F, 0.5F);
+		}
+		super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
 	}
 
 	@Override
