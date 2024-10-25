7cb20bd7948ceaa3c6f9f3e26cf9f9dc63862d06
Coda <cody.koelliker@outlook.com>
2024-10-25 17:48:59 -0600

    desert baboab leaves tint now

--- src/main/java/coda/croodaceous/client/ClientEvents.java
+++ src/main/java/coda/croodaceous/client/ClientEvents.java
@@ -2,10 +2,19 @@ package coda.croodaceous.client;
 
 import coda.croodaceous.CroodaceousMod;
 import coda.croodaceous.client.render.*;
+import coda.croodaceous.registry.CEBlocks;
 import coda.croodaceous.registry.CEEntities;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.item.ItemColor;
+import net.minecraft.client.color.item.ItemColors;
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.level.FoliageColor;
+import net.minecraft.world.level.GrassColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,11 +23,23 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ClientEvents {
 
     @SubscribeEvent
-    public static void clientSetup(final FMLClientSetupEvent e) {
+    public static void clientSetup(FMLClientSetupEvent e) {
+
     }
-    
+
     @SubscribeEvent
-    public static void onRegisterRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+    public static void registerBlockColors(RegisterColorHandlersEvent.Block e) {
+        e.register((p_92621_, level, pos, p_92624_) -> level != null && pos != null ? BiomeColors.getAverageGrassColor(level, pos) : GrassColor.get(0.9D, 0.1D), CEBlocks.DESERT_BAOBAB_LEAVES.get());
+    }
+
+
+    @SubscribeEvent
+    public static void registerItemColors(RegisterColorHandlersEvent.Item e) {
+        e.register((p1, p2) -> GrassColor.get(0.9D, 0.1D), CEBlocks.DESERT_BAOBAB_LEAVES.get().asItem());
+    }
+
+    @SubscribeEvent
+    public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(CEEntities.LIYOTE.get(), LiyoteRenderer::new);
         event.registerEntityRenderer(CEEntities.BEAROWL.get(), BearOwlRenderer::new);
         event.registerEntityRenderer(CEEntities.RAMU.get(), RamuRenderer::new);
